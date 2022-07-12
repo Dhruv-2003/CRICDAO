ef9c4e22505e58db3d5c34edde773ee4ec079fca
dhruv <dhruvagarwal2017@gmail.com>
2022-07-13 00:51:06 +0530

    Package updates

--- CRICDAO/my-app/pages/account.js
+++ CRICDAO/my-app/pages/account.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
-import { currentAccount } from "../components/ConnectWallet";
+// import { currentAccount } from "../components/ConnectWallet";
 import styles from "../styles/Account.module.css";
-import { useMoralisWeb3Api } from "react-moralis";
+// import { useMoralisWeb3Api } from "react-moralis";
 import Card from "../components/Card";
 import { ethers } from "ethers";
 
 const account = () => {
-  const contract_address = "";
-  // const Web3Api = useMoralisWeb3Api();
-  // const [NFTs,setNFTs] = useState("");
-  // const [counter,setCounter] = useState(0) ;
-  const price = 0.01;
+  // const contract_address = "";
+  // // const Web3Api = useMoralisWeb3Api();
+  // // const [NFTs,setNFTs] = useState("");
+  // // const [counter,setCounter] = useState(0) ;
+  // const price = 0.01;
 
   // const fetchNFTsForContract = async () => {
   //   const options = {
@@ -24,14 +24,14 @@ const account = () => {
   return (
     <div>
       {/* card section */}
-      <div className={styles.card}>
+      {/* <div className={styles.card}>
         <div className={styles.cardmain}>
           <img src="/avatar.png" className={styles.cardimg} />
           <div className={styles.account}>Account:{currentAccount}</div>
           <div className={styles.account}>Balance: {}</div>
           <div className={styles.account}>NFT Players Owned: {counter}</div>
         </div>
-      </div>
+      </div> */}
 
       {/* NFTs Section
       <div className={styles.nfts} >
